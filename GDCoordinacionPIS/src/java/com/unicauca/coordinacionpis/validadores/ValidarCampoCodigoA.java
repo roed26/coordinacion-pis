b22008f62ad5a7f245fcf4a0ec0dd601022de293
paolasso <paolasso68@gmail.com>
2017-04-17 20:55:07 -0500

    registro materias
    se implementó el crud de materias para la oferta académica

---
+++ GDCoordinacionPIS/src/java/com/unicauca/coordinacionpis/validadores/ValidarCampoCodigoA.java
@@ -0,0 +1,33 @@
+package com.unicauca.coordinacionpis.validadores;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+
+@FacesValidator(value="ValidarCampoCodigoA")
+public class ValidarCampoCodigoA implements Validator
+{
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException 
+    {
+        String texto = String.valueOf(value);
+        
+       if(texto.length()>30){
+           FacesMessage msg= new FacesMessage(FacesMessage.SEVERITY_ERROR,"","Máximo treinta caracteres");
+           throw new ValidatorException(msg);
+        }
+        
+        
+
+    }
+    
+    
+    
+    
+    
+}
