01870bc0178e6146ab56e47ef85ef156d4d0909e
juandiazunicauca <juanmos@unicauca.edu.co>
2017-04-19 10:06:09 -0500

    MetadatosAnteproyecto
    se ha agregado la clase MetadatosAnteproyecto de la HU35

---
+++ GDCoordinacionPIS/src/java/com/unicauca/coordinacionpis/classMetadatos/MetadatosAntepoyecto.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unicauca.coordinacionpis.classMetadatos;
+
+/**
+ *
+ * @author Andrés
+ */
+public class MetadatosAntepoyecto {
+    
+    private String profesor;
+    private String titulo;
+    private String fecha;
+    private String viabilidad;
+    private String actaAprobacion;
+    private String nombreEstudiante1;
+    private String nombreEstudiante2;
+    
+
+    public String getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(String profesor) {
+        this.profesor = profesor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getActaAprobacion() {
+        return actaAprobacion;
+    }
+
+    public void setActaAprobacion(String actaAprobacion) {
+        this.actaAprobacion = actaAprobacion;
+    }
+
+    public String getNombreEstudiante1() {
+        return nombreEstudiante1;
+    }
+
+    public void setNombreEstudiante1(String nombreEstudiante1) {
+        this.nombreEstudiante1 = nombreEstudiante1;
+    }
+
+    public String getNombreEstudiante2() {
+        return nombreEstudiante2;
+    }
+
+    public void setNombreEstudiante2(String nombreEstudiante2) {
+        this.nombreEstudiante2 = nombreEstudiante2;
+    }
+    
+    
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getViabilidad() {
+        return viabilidad;
+    }
+
+    public void setViabilidad(String viabilidad) {
+        this.viabilidad = viabilidad;
+    }
+    
+    
+    
+    
+}
