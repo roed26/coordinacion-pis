cc04c0ac01fc00aabba409479977631444c3a54e
Jeison12 <alexander-1251991@hotmail.com>
2017-05-28 21:39:39 -0500

    Editar plan de estudio
    Implementación funcionalidad editar plan de estudio.

--- GDCoordinacionPIS/src/java/com/unicauca/coordinacionpis/managedbean/RegistroPlandeEstudioController.java
+++ GDCoordinacionPIS/src/java/com/unicauca/coordinacionpis/managedbean/RegistroPlandeEstudioController.java
@@ -75,6 +75,11 @@ public class RegistroPlandeEstudioController implements Serializable {
     private BufferedOutputStream output;
     private BufferedInputStream input;
     private SimpleDateFormat formatoFecha;
+    private String documentoAnterior;
+    private String rutaPlanesDeEstudio;
+    private int auxNumeroPlan;
+    private String auxAcuerdoPlan;
+    private Date auxFechaPlan;
 
     private OKMWebservices okm;
 
@@ -86,6 +91,12 @@ public class RegistroPlandeEstudioController implements Serializable {
         this.exitoSubirArchivo = false;
         this.conexionOpenKM = new ConexionOpenKM();
         this.okm = conexionOpenKM.getOkm();
+        this.documentoAnterior = "";
+        this.rutaPlanesDeEstudio = "/okm:root/Planes de Estudio";
+        auxNumeroPlan = 1;
+        auxAcuerdoPlan = "";
+        auxFechaPlan = null;
+
     }
 
     public String getNombreArchivo() {
@@ -155,6 +166,7 @@ public class RegistroPlandeEstudioController implements Serializable {
      */
     public void seleccionarArchivo(FileUploadEvent event) {
         nombreArchivo = event.getFile().getFileName();
+        System.out.println("nombre archivo: " + nombreArchivo);
         archivoPlan = event.getFile();
         FacesMessage msg = new FacesMessage("El archivo", nombreArchivo + " se seleccionó con exito");
         FacesContext.getCurrentInstance().addMessage(null, msg);
@@ -167,13 +179,27 @@ public class RegistroPlandeEstudioController implements Serializable {
 
     }
 
+    public void seleccionarArchivoActualizacion(FileUploadEvent event) {
+        nombreArchivo = event.getFile().getFileName();
+        System.out.println("nombre archivo: " + nombreArchivo);
+        archivoPlan = event.getFile();
+        FacesMessage mensaje = new FacesMessage("El archivo", nombreArchivo + " se seleccionó con exito");
+        FacesContext.getCurrentInstance().addMessage(null, mensaje);
+        RequestContext rc = RequestContext.getCurrentInstance();
+        rc.update("msg");//Actualiza la etiqueta growl para que el mensaje pueda ser mostrado
+        exitoSubirArchivo = true;
+        rc.update("formActualizarArchivoPlanEstudio");
+        rc.update("formArchivoSelecionadoActualizarPlanEstudio");
+        rc.update("formActualizarMetadatosPlanEstudio");
+
+    }
+
     public void aceptarRegistroPlanEstudio() {
         RequestContext rc = RequestContext.getCurrentInstance();
         FacesMessage message = null;
 
         try {
-//            listaDocs();
-//            rc.update("lstPlanesEstudio");
+
             boolean existeFolder = false;
             boolean existeDocumento = false;
 
@@ -188,18 +214,18 @@ public class RegistroPlandeEstudioController implements Serializable {
             }
             if (existeFolder) {//Si existe la carpeta Planes de Estudio, crea dentro de ella el documento
                 if (!existeDocumento) {
-                    okm.createDocumentSimple("/okm:root/Planes de Estudio/" + nombreArchivo, archivoPlan.getInputstream());//Crear el documento en openkm                    
-                    okm.addKeyword("/okm:root/Planes de Estudio/" + nombreArchivo, "" + metadatosPlandeEstudio.getNumero());
-                    okm.addKeyword("/okm:root/Planes de Estudio/" + nombreArchivo, "" + metadatosPlandeEstudio.getAcuerdo());
-                    okm.addKeyword("/okm:root/Planes de Estudio/" + nombreArchivo, "" + formatoFecha.format(metadatosPlandeEstudio.getVigencia()));
+                    okm.createDocumentSimple(rutaPlanesDeEstudio + "/" + nombreArchivo, archivoPlan.getInputstream());//Crear el documento en openkm                    
+                    okm.addKeyword(rutaPlanesDeEstudio + "/" + nombreArchivo, "" + metadatosPlandeEstudio.getNumero());
+                    okm.addKeyword(rutaPlanesDeEstudio + "/" + nombreArchivo, "" + metadatosPlandeEstudio.getAcuerdo());
+                    okm.addKeyword(rutaPlanesDeEstudio + "/" + nombreArchivo, "" + formatoFecha.format(metadatosPlandeEstudio.getVigencia()));
 
                     message = new FacesMessage("El archivo", nombreArchivo + " se registro con exito");
                 } else {
                     message = new FacesMessage("Error al registrar el archivo", nombreArchivo);
                 }
             } else {//Si la carpeta no existe, la crea y dentro de ella crea el documento.
-                okm.createFolderSimple("/okm:root/Planes de Estudio");//Crear carpeta Planes de Estudio en openkm
-                okm.createDocumentSimple("/okm:root/Planes de Estudio/" + nombreArchivo, archivoPlan.getInputstream());//Crear el documento dentro de la carpeta Planes de Estudio en openkm
+                okm.createFolderSimple(rutaPlanesDeEstudio);//Crear carpeta Planes de Estudio en openkm
+                okm.createDocumentSimple(rutaPlanesDeEstudio + "/" + nombreArchivo, archivoPlan.getInputstream());//Crear el documento dentro de la carpeta Planes de Estudio en openkm
                 message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "El archivo " + nombreArchivo + " se registro con exito");
             }
             FacesContext.getCurrentInstance().addMessage(null, message);
@@ -216,7 +242,6 @@ public class RegistroPlandeEstudioController implements Serializable {
             rc.update("lstPlanesEstudio");
             rc.execute("PF('dlgRegistroPlandeEstudio').hide()");//Cerrar el dialog que contiene el formulario
 
-//            rc.execute("PF('mensajeInformacionPlanEstudio').show()");
         } catch (PathNotFoundException ex) {
             Logger.getLogger(RegistroOfertaAcademicaController.class.getName()).log(Level.SEVERE, null, ex);
         } catch (RepositoryException ex) {
@@ -255,20 +280,30 @@ public class RegistroPlandeEstudioController implements Serializable {
     public void cargarPlanEstudio(Document document) {
         try {
             RequestContext rc = RequestContext.getCurrentInstance();
-
+//            this.exitoSubirArchivo = true;
+//            rc.update("formActualizarArchivoPlanEstudio");
+//            rc.update("formArchivoSelecionadoActualizarPlanEstudio");
+//            rc.update("formActualizarMetadatosPlanEstudio");
             Set<String> palabras = document.getKeywords();
             for (int i = 0; i < palabras.size(); i++) {
                 if (Validador.esFecha((String) palabras.toArray()[i])) {
                     String fecha = (String) palabras.toArray()[i];
                     metadatosPlandeEstudio.setVigencia(formatoFecha.parse(fecha));
+                    auxFechaPlan = metadatosPlandeEstudio.getVigencia();
                 } else if (Validador.esNumero((String) palabras.toArray()[i])) {
                     metadatosPlandeEstudio.setNumero(Integer.parseInt((String) palabras.toArray()[i]));
+                    auxNumeroPlan = metadatosPlandeEstudio.getNumero();
                 } else {
                     metadatosPlandeEstudio.setAcuerdo((String) palabras.toArray()[i]);
+                    auxAcuerdoPlan = metadatosPlandeEstudio.getAcuerdo();
                 }
             }
 
             nombreArchivo = nombreDelArchivo(document.getPath());
+            documentoAnterior = nombreArchivo;
+            this.exitoSubirArchivo = true;
+
+            rc.update("formActualizarArchivoPlanEstudio");
             rc.update("formArchivoSelecionadoActualizarPlanEstudio");
             rc.update("formActualizarMetadatosPlanEstudio");
         } catch (java.text.ParseException ex) {
@@ -278,6 +313,81 @@ public class RegistroPlandeEstudioController implements Serializable {
 
     public void editarPlanEstudio() {
 
+        RequestContext rc = RequestContext.getCurrentInstance();
+        FacesMessage message = null;
+
+        try {
+            if (!nombreArchivo.equals(documentoAnterior)) {
+                okm.deleteDocument(rutaPlanesDeEstudio + "/" + documentoAnterior);
+                okm.createDocumentSimple(rutaPlanesDeEstudio + "/" + nombreArchivo, archivoPlan.getInputstream());//Crear el documento en openkm   
+                okm.addKeyword(rutaPlanesDeEstudio + "/" + nombreArchivo, "" + metadatosPlandeEstudio.getNumero());
+                okm.addKeyword(rutaPlanesDeEstudio + "/" + nombreArchivo, "" + metadatosPlandeEstudio.getAcuerdo());
+                okm.addKeyword(rutaPlanesDeEstudio + "/" + nombreArchivo, "" + formatoFecha.format(metadatosPlandeEstudio.getVigencia()));
+                message = new FacesMessage("Plan de estudio actualizado correctamente");
+            } else {
+                if (metadatosPlandeEstudio.getNumero() != auxNumeroPlan) {
+                    okm.removeKeyword(rutaPlanesDeEstudio + "/" + nombreArchivo, "" + auxNumeroPlan);
+                    okm.addKeyword(rutaPlanesDeEstudio + "/" + nombreArchivo, "" + metadatosPlandeEstudio.getNumero());
+                    message = new FacesMessage("Plan de estudio actualizado correctamente");
+                }
+                if (!metadatosPlandeEstudio.getAcuerdo().equalsIgnoreCase(auxAcuerdoPlan)) {
+                    okm.removeKeyword(rutaPlanesDeEstudio + "/" + nombreArchivo, "" + auxAcuerdoPlan);
+                    okm.addKeyword(rutaPlanesDeEstudio + "/" + nombreArchivo, "" + metadatosPlandeEstudio.getAcuerdo());
+                    message = new FacesMessage("Plan de estudio actualizado correctamente");
+                }
+                if (metadatosPlandeEstudio.getVigencia().compareTo(auxFechaPlan) != 0) {
+                    okm.removeKeyword(rutaPlanesDeEstudio + "/" + nombreArchivo, "" + formatoFecha.format(auxFechaPlan));
+                    okm.addKeyword(rutaPlanesDeEstudio + "/" + nombreArchivo, "" + formatoFecha.format(metadatosPlandeEstudio.getVigencia()));
+                    message = new FacesMessage("Plan de estudio actualizado correctamente");
+                }
+            }
+            FacesContext.getCurrentInstance().addMessage(null, message);
+            rc.update("formMetadatosPlanEstudio");//Actualizar el formulario de registro
+            rc.execute("PF('dlgEditarPlanEstudio').hide()");//Cerrar el dialog que contiene el formulario
+
+            limpiarVariables();
+
+            rc.update("formActualizarArchivoPlanEstudio");
+            rc.update("formArchivoSelecionadoActualizarPlanEstudio");
+            rc.update("formActualizarMetadatosPlanEstudio");//Actualizar el formulario de registro
+
+            listaDocs();
+            rc.update("lstPlanesEstudio");
+            rc.execute("PF('dlgEditarPlanEstudio').hide()");//Cerrar el dialog que contiene el formulario
+
+        } catch (AccessDeniedException ex) {
+            Logger.getLogger(RegistroPlandeEstudioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (RepositoryException ex) {
+            Logger.getLogger(RegistroPlandeEstudioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (PathNotFoundException ex) {
+            Logger.getLogger(RegistroPlandeEstudioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (LockException ex) {
+            Logger.getLogger(RegistroPlandeEstudioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (DatabaseException ex) {
+            Logger.getLogger(RegistroPlandeEstudioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ExtensionException ex) {
+            Logger.getLogger(RegistroPlandeEstudioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnknowException ex) {
+            Logger.getLogger(RegistroPlandeEstudioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (WebserviceException ex) {
+            Logger.getLogger(RegistroPlandeEstudioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(RegistroPlandeEstudioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedMimeTypeException ex) {
+            Logger.getLogger(RegistroPlandeEstudioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (FileSizeExceededException ex) {
+            Logger.getLogger(RegistroPlandeEstudioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UserQuotaExceededException ex) {
+            Logger.getLogger(RegistroPlandeEstudioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (VirusDetectedException ex) {
+            Logger.getLogger(RegistroPlandeEstudioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ItemExistsException ex) {
+            Logger.getLogger(RegistroPlandeEstudioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (AutomationException ex) {
+            Logger.getLogger(RegistroPlandeEstudioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (VersionException ex) {
+            Logger.getLogger(RegistroPlandeEstudioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -287,7 +397,7 @@ public class RegistroPlandeEstudioController implements Serializable {
         try {
             if (existeCarpeta()) {
                 documentosPlanEstudio.clear();
-                documentosPlanEstudio = okm.getDocumentChildren("/okm:root/Planes de Estudio");//Obtener los Planes de Estudio contenidos en openkm          
+                documentosPlanEstudio = okm.getDocumentChildren(rutaPlanesDeEstudio);//Obtener los Planes de Estudio contenidos en openkm          
             }
         } catch (RepositoryException ex) {
             Logger.getLogger(RegistroOfertaAcademicaController.class.getName()).log(Level.SEVERE, null, ex);
@@ -371,7 +481,7 @@ public class RegistroPlandeEstudioController implements Serializable {
     private boolean existeCarpeta() throws PathNotFoundException, RepositoryException, DatabaseException, UnknowException, WebserviceException {
 
         for (Folder fld : okm.getFolderChildren("/okm:root")) {
-            if (fld.getPath().equalsIgnoreCase("/okm:root/Planes de Estudio")) {//Buscar en openkm si existe la carpeta Planes de Estudio
+            if (fld.getPath().equalsIgnoreCase(rutaPlanesDeEstudio)) {//Buscar en openkm si existe la carpeta Planes de Estudio
                 return true;
             }
         }
@@ -458,6 +568,15 @@ public class RegistroPlandeEstudioController implements Serializable {
         rc.update("formSeleccionarArchivoPlanEstudio");
         rc.update("formArchivoSelecionadoPlanEstudio");
         rc.update("formMetadatosPlanEstudio");
+
+    }
+
+    public void cambiarArchivoActualizacion() {
+        exitoSubirArchivo = false;
+        RequestContext rc = RequestContext.getCurrentInstance();
+        rc.update("formActualizarArchivoPlanEstudio");
+        rc.update("formArchivoSelecionadoActualizarPlanEstudio");
+        rc.update("formActualizarMetadatosPlanEstudio");
 
     }
 
